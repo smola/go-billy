c249b2d21e9afab6594b6375c80644a2a29f1577
Santiago M. Mola <santi@mola.io>
2017-01-04 18:27:59 +0100

    add CopyFile function.

--- utils.go
+++ utils.go
@@ -1,6 +1,49 @@
 package billy
 
-import "os"
+import (
+	"io"
+	"os"
+)
+
+// CopyFile copies a file across filesystems. If there is any error when
+// opening, copying or closing any of the files, it tries to remove the
+// destination file.
+func CopyFile(src, dst Filesystem, srcPath, dstPath string) error {
+	srcFile, err := src.Open(srcPath)
+	if err != nil {
+		return err
+	}
+
+	dstFile, err := dst.Create(dstPath)
+	if err != nil {
+		_ = srcFile.Close()
+		_ = dst.Remove(dstPath)
+		return err
+	}
+
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		_ = dstFile.Close()
+		_ = dst.Remove(dstPath)
+		_ = srcFile.Close()
+		return err
+	}
+
+	if err := srcFile.Close(); err != nil {
+		_ = dstFile.Close()
+		_ = dst.Remove(dstPath)
+		return err
+	}
+
+	err = dstFile.Close()
+	if err != nil {
+		_ = dst.Remove(dstPath)
+		_ = srcFile.Close()
+		return err
+	}
+
+	return nil
+}
 
 // Exists returns true if the path exists in the filesystem. False, otherwise.
 // If there is an I/O error that prevents checking the existence of the file
--- utils_test.go
+++ utils_test.go
@@ -2,12 +2,14 @@ package billy_test
 
 import (
 	"errors"
+	"os"
 	"testing"
 
 	"srcd.works/go-billy.v1"
 	"srcd.works/go-billy.v1/memory"
 
 	. "gopkg.in/check.v1"
+	"io/ioutil"
 )
 
 func Test(t *testing.T) { TestingT(t) }
@@ -15,6 +17,115 @@ func Test(t *testing.T) { TestingT(t) }
 type UtilsSuite struct{}
 
 var _ = Suite(&UtilsSuite{})
+
+func (s *UtilsSuite) TestCopyFile(c *C) {
+	src := memory.New()
+	dst := memory.New()
+	path := "path"
+	f, err := src.Create(path)
+	c.Assert(err, IsNil)
+	_, err = f.Write([]byte("foo"))
+	c.Assert(err, IsNil)
+	c.Assert(f.Close(), IsNil)
+
+	err = billy.CopyFile(src, dst, path, path)
+	c.Assert(err, IsNil)
+	f, err = dst.Open(path)
+	c.Assert(err, IsNil)
+	b, err := ioutil.ReadAll(f)
+	c.Assert(err, IsNil)
+	c.Assert(string(b), Equals, "foo")
+	c.Assert(f.Close(), IsNil)
+}
+
+func (s *UtilsSuite) TestCopyFileNonExistentSource(c *C) {
+	src := memory.New()
+	dst := memory.New()
+	path := "path"
+
+	err := billy.CopyFile(src, dst, path, path)
+	c.Assert(os.IsNotExist(err), Equals, true)
+	exists, err := billy.Exists(dst, path)
+	c.Assert(err, IsNil)
+	c.Assert(exists, Equals, false)
+}
+
+func (s *UtilsSuite) TestCopyFileNonWriteableDest(c *C) {
+	src := memory.New()
+	dst := &errorCreateFs{memory.New(), false}
+	path := "path"
+	f, err := src.Create(path)
+	c.Assert(err, IsNil)
+	c.Assert(f.Close(), IsNil)
+
+	err = billy.CopyFile(src, dst, path, path)
+	c.Assert(err, ErrorMatches, "test error")
+	exists, err := billy.Exists(dst, path)
+	c.Assert(err, IsNil)
+	c.Assert(exists, Equals, false)
+}
+
+func (s *UtilsSuite) TestCopyFileCreateError(c *C) {
+	src := memory.New()
+	dst := &errorCreateFs{memory.New(), true}
+	path := "path"
+	f, err := src.Create(path)
+	c.Assert(err, IsNil)
+	c.Assert(f.Close(), IsNil)
+
+	err = billy.CopyFile(src, dst, path, path)
+	c.Assert(err, ErrorMatches, "test error")
+	exists, err := billy.Exists(dst, path)
+	c.Assert(err, IsNil)
+	c.Assert(exists, Equals, false)
+}
+
+func (s *UtilsSuite) TestCopyFileReaderError(c *C) {
+	src := &badReaderFs{memory.New()}
+	dst := memory.New()
+	path := "path"
+	f, err := src.Create(path)
+	c.Assert(err, IsNil)
+	c.Assert(f.Close(), IsNil)
+
+	err = billy.CopyFile(src, dst, path, path)
+	c.Assert(err, ErrorMatches, "test error")
+	exists, err := billy.Exists(dst, path)
+	c.Assert(err, IsNil)
+	c.Assert(exists, Equals, false)
+}
+
+func (s *UtilsSuite) TestCopyFileSourceCloseError(c *C) {
+	var src billy.Filesystem = memory.New()
+	dst := memory.New()
+	path := "path"
+	f, err := src.Create(path)
+	c.Assert(err, IsNil)
+	c.Assert(f.Close(), IsNil)
+	src = &badCloserFs{src}
+
+	err = billy.CopyFile(src, dst, path, path)
+	c.Assert(err, ErrorMatches, "test error")
+	exists, err := billy.Exists(dst, path)
+	c.Assert(err, IsNil)
+	c.Assert(exists, Equals, false)
+}
+
+func (s *UtilsSuite) TestCopyFileDestCloseError(c *C) {
+	src := memory.New()
+	var dst billy.Filesystem = memory.New()
+	path := "path"
+	f, err := src.Create(path)
+	c.Assert(err, IsNil)
+	c.Assert(f.Close(), IsNil)
+	dst = &badCloserFs{dst}
+
+	err = billy.CopyFile(src, dst, path, path)
+	c.Assert(err, ErrorMatches, "test error")
+	exists, err := billy.Exists(dst, path)
+	c.Assert(err, IsNil)
+	c.Assert(exists, Equals, false)
+}
 
 func (s *UtilsSuite) TestExistsFalse(c *C) {
 	fs := memory.New()
@@ -49,4 +160,68 @@ type errorStatFs struct {
 
 func (fs *errorStatFs) Stat(path string) (billy.FileInfo, error) {
 	return nil, errors.New("test error")
+}
+
+type errorCreateFs struct {
+	billy.Filesystem
+	do bool
+}
+
+func (fs *errorCreateFs) Create(path string) (billy.File, error) {
+	if fs.do {
+		_, _ = fs.Filesystem.Create(path)
+	}
+
+	return nil, errors.New("test error")
+}
+
+type badReaderFs struct {
+	billy.Filesystem
+}
+
+func (fs *badReaderFs) Open(path string) (billy.File, error) {
+	f, err := fs.Filesystem.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return &badReadFile{f}, nil
+}
+
+type badReadFile struct {
+	billy.File
+}
+
+func (f *badReadFile) Read(b []byte) (int, error) {
+	return 0, errors.New("test error")
+}
+
+type badCloserFs struct {
+	billy.Filesystem
+}
+
+func (fs *badCloserFs) Open(path string) (billy.File, error) {
+	f, err := fs.Filesystem.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return &badCloserFile{f}, nil
+}
+
+func (fs *badCloserFs) Create(path string) (billy.File, error) {
+	f, err := fs.Filesystem.Create(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return &badCloserFile{f}, nil
+}
+
+type badCloserFile struct {
+	billy.File
+}
+
+func (f *badCloserFile) Close() error {
+	return errors.New("test error")
 }
